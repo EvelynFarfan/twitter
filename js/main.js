f485d18d2daa292128c84a36a0cc014cf6dbbd79
efarfan2393 <e.farfanvaldivia@gmail.com>
2016-09-20 15:41:58 -0500

    subiendo version 0.0.2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 window.addEventListener("load", function() {
-	var boton = document.getElementById("boton");
-	boton.addEventListener("click", function(){
-		var textoInput = document.getElementById("textoInput").value;
-		var publicacion = document.createElement("div");
-		publicacion.innerHTML = textoInput;
-		var contenido = document.getElementById("mensaje");
-		contenido.insertBefore(publicacion, contenido.childNodes[0]);
-		document.getElementById("textoInput").value = "";
+		var boton = document.getElementById("boton");
+		var textoInput = document.getElementById("textoInput");
+		var numCaratMax = 140;
+		var contador = document.getElementById("counter");
+		contador.innerHTML = 140;
+		boton.disabled = true;
+
+		boton.addEventListener("click", function(e){
+			// e.preventDefault();
+			var publicacion = document.createElement("p");
+			publicacion.classList.add("publicacion");
+			publicacion.innerHTML = textoInput.value;
+			var contenido = document.getElementById("mensaje");
+			contenido.insertBefore(publicacion, contenido.childNodes[0]);
+			document.getElementById("textoInput").value = "";
+			contador.innerHTML = 140;
+			boton.disabled = true;
 		});
-});
+
+		textoInput.addEventListener("keyup", function(){
+			boton.disabled = false;
+			var numCarat = textoInput.value.length;
+			contador.innerHTML = numCaratMax - numCarat;
+			if(numCarat === 0){
+				boton.disabled = true;
+			}
+		});
+	});
+
+
+
+
+
